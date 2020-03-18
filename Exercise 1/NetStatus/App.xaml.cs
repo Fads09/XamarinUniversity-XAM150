0de56477b35048fee8025f7d742357f07a13e008
Fadoua Amrou <faitamrou.90@gmail.com>
2020-03-18 17:22:10 -0400

    XAM150 - Consuming REST-based Web Services
    This repository contains exercises for the Xamarin University XAM150 - Consuming REST-based Web Services training course.

---
+++ Exercise 1/NetStatus/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NetStatus
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = CrossConnectivity.Current.IsConnected
+                ? (Page) new NetworkViewPage()
+                : new NoNetworkPage();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            CrossConnectivity.Current.ConnectivityChanged += HandleConnectionChange;
+        }
+
+        private void HandleConnectionChange(object sender, ConnectivityChangedEventArgs e)
+        {
+            Type currentPage = this.MainPage.GetType();
+            if(e.IsConnected && currentPage != typeof(NetworkViewPage))
+            {
+                this.MainPage = new NetworkViewPage();
+            }
+            else if(!e.IsConnected && currentPage != typeof(NoNetworkPage))
+            {
+                this.MainPage = new NetworkViewPage();
+            }
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Exercise 1/NetStatus/NetworkViewPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
+using Xamarin.Forms;
+
+namespace NetStatus
+{
+    public partial class NetworkViewPage : ContentPage
+    {
+        public NetworkViewPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if(CrossConnectivity.Current.ConnectionTypes != null)
+            {
+                ConnectionDetails.Text = CrossConnectivity.Current.ConnectionTypes.First().ToString();
+                CrossConnectivity.Current.ConnectivityChanged += UpdateNetworkInfo;
+            }
+            else
+            {
+                ConnectionDetails.Text = "No Network";
+            }
+            
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            if (CrossConnectivity.Current != null)
+                CrossConnectivity.Current.ConnectivityChanged -= UpdateNetworkInfo;
+        }
+
+        private void UpdateNetworkInfo(object sender, ConnectivityChangedEventArgs e)
+        {
+            if (CrossConnectivity.Current != null && CrossConnectivity.Current.ConnectionTypes != null)
+            {
+                var connectionType = CrossConnectivity.Current.ConnectionTypes.FirstOrDefault();
+                ConnectionDetails.Text = connectionType.ToString();
+            }
+        }
+    }
+}
